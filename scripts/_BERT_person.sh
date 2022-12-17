5a9bfa78f296e879746438f8d8cc42ba75db01b6
brjathu <brjathu@gmail.com>
2022-12-16 19:07:40 -0800

    pose only multi-person model sweep 3000_M1, 3000_m2

--- scripts/_BERT_person.sh
+++ scripts/_BERT_person.sh
@@ -72,44 +72,132 @@
 
 
 
-python src/train.py -m \
-hydra/launcher=submitit_slurm \
-launcher=slurm \
-trainer=ddp \
-task_name=3000_P3 \
-configs.dataset_loader=fast \
-configs.train_dataset=\'kinetics_train_7,ava_train_7\' \
-configs.test_dataset=\'ava_val_8\' \
-configs.ava.predict_valid=True \
-configs.bottle_neck="conv2k" \
-configs.ava.gt_type="all" \
-configs.ava.distil_type="both_bce" \
-configs.lr=1e-3,2e-3,4e-3 \
-configs.mask_type_test="zero" \
-configs.frame_length=125 \
-configs.max_people=1 \
-configs.loss_type=\'action_bce\' \
-configs.extra_feat.enable=\'mvit\' \
-configs.extra_feat.mvit.mid_dim=1024 \
-configs.extra_feat.mvit.en_dim=1024 \
-configs.extra_feat.pose_shape.mid_dim=229 \
-configs.extra_feat.pose_shape.en_dim=256 \
-configs.extra_feat.relative_pose.mid_dim=512 \
-configs.extra_feat.relative_pose.en_dim=1280 \
-configs.use_relative_pose=True \
-configs.in_feat=1280 \
-configs.weight_decay=5e-2 \
-trainer.gradient_clip_val=2.0 \
-configs.mask_ratio=0.1 \
-configs.masked=False \
-configs.vit.depth=3,9 \
-configs.vit.heads=8,12 \
-configs.vit.mlp_dim=128,512 \
-configs.vit.dim_head=32,128 \
-configs.vit.dropout=0.1 \
-configs.vit.droppath=0.1 \
+# python src/train.py -m \
+# hydra/launcher=submitit_slurm \
+# launcher=slurm \
+# trainer=ddp \
+# task_name=3000_P3 \
+# configs.dataset_loader=fast \
+# configs.train_dataset=\'kinetics_train_7,ava_train_7\' \
+# configs.test_dataset=\'ava_val_8\' \
+# configs.ava.predict_valid=True \
+# configs.bottle_neck="conv2k" \
+# configs.ava.gt_type="all" \
+# configs.ava.distil_type="both_bce" \
+# configs.lr=1e-3,2e-3,4e-3 \
+# configs.mask_type_test="zero" \
+# configs.frame_length=125 \
+# configs.max_people=1 \
+# configs.loss_type=\'action_bce\' \
+# configs.extra_feat.enable=\'mvit\' \
+# configs.extra_feat.mvit.mid_dim=1024 \
+# configs.extra_feat.mvit.en_dim=1024 \
+# configs.extra_feat.pose_shape.mid_dim=229 \
+# configs.extra_feat.pose_shape.en_dim=256 \
+# configs.extra_feat.relative_pose.mid_dim=512 \
+# configs.extra_feat.relative_pose.en_dim=1280 \
+# configs.use_relative_pose=True \
+# configs.in_feat=1280 \
+# configs.weight_decay=5e-2 \
+# trainer.gradient_clip_val=2.0 \
+# configs.mask_ratio=0.1 \
+# configs.masked=False \
+# configs.vit.depth=3,9 \
+# configs.vit.heads=8,12 \
+# configs.vit.mlp_dim=128,512 \
+# configs.vit.dim_head=32,128 \
+# configs.vit.dropout=0.1 \
+# configs.vit.droppath=0.1 \
 
 
+
+
+
+
+# python src/train.py -m \
+# hydra/launcher=submitit_slurm \
+# launcher=slurm \
+# trainer=ddp \
+# task_name=3000_F1 \
+# configs.wights_path="logs/3000_P2/10/checkpoints/last.ckpt" \
+# configs.dataset_loader=fast \
+# configs.train_dataset=\'ava_train_9\' \
+# configs.test_dataset=\'ava_val_9\' \
+# configs.ava.predict_valid=True \
+# configs.bottle_neck="conv2k" \
+# configs.ava.gt_type="gt" \
+# configs.lr=1e-5,5e-5,1e-4 \
+# configs.mask_type_test="zero" \
+# configs.frame_length=125 \
+# configs.max_people=1 \
+# configs.loss_type=\'action_bce\' \
+# configs.extra_feat.enable=\'mvit\' \
+# configs.extra_feat.mvit.mid_dim=1024 \
+# configs.extra_feat.mvit.en_dim=1024 \
+# configs.extra_feat.pose_shape.mid_dim=229 \
+# configs.extra_feat.pose_shape.en_dim=256 \
+# configs.extra_feat.relative_pose.mid_dim=512 \
+# configs.extra_feat.relative_pose.en_dim=1280 \
+# configs.use_relative_pose=True \
+# configs.in_feat=1280 \
+# configs.weight_decay=5e-2 \
+# trainer.gradient_clip_val=2,5,200.0 \
+# configs.mask_ratio=0.1,0.2,0.4 \
+# configs.masked=False \
+# trainer.max_epochs=30 \
+# configs.mixed_training=4 \
+# configs.test_type="track.fullframe|avg.6" \
+# configs.vit.droppath=0.1,0.3 \
+# configs.layer_decay=0.9,0.8 \
+# # configs.debug=True \
+# # trainer.devices=1 \
+# # callbacks.rich_progress_bar.refresh_rate=0 \
+
+
+
+
+# python src/train.py -m \
+# hydra/launcher=submitit_slurm \
+# launcher=slurm \
+# trainer=ddp \
+# task_name=3000_F2 \
+# configs.wights_path="logs/3000_P3/26/checkpoints/last.ckpt" \
+# configs.dataset_loader=fast \
+# configs.train_dataset=\'ava_train_9\' \
+# configs.test_dataset=\'ava_val_9\' \
+# configs.ava.predict_valid=True \
+# configs.bottle_neck="conv2k" \
+# configs.ava.gt_type="gt" \
+# configs.lr=1e-5,5e-5,1e-4 \
+# configs.mask_type_test="zero" \
+# configs.frame_length=125 \
+# configs.max_people=1 \
+# configs.loss_type=\'action_bce\' \
+# configs.extra_feat.enable=\'mvit\' \
+# configs.extra_feat.mvit.mid_dim=1024 \
+# configs.extra_feat.mvit.en_dim=1024 \
+# configs.extra_feat.pose_shape.mid_dim=229 \
+# configs.extra_feat.pose_shape.en_dim=256 \
+# configs.extra_feat.relative_pose.mid_dim=512 \
+# configs.extra_feat.relative_pose.en_dim=1280 \
+# configs.use_relative_pose=True \
+# configs.in_feat=1280 \
+# configs.weight_decay=5e-2 \
+# trainer.gradient_clip_val=2,5,200.0 \
+# configs.mask_ratio=0.1,0.2,0.4 \
+# configs.masked=False \
+# trainer.max_epochs=30 \
+# configs.mixed_training=4 \
+# configs.test_type="track.fullframe|avg.6" \
+# configs.vit.droppath=0.1,0.3 \
+# configs.layer_decay=0.9,0.8 \
+# configs.vit.depth=9 \
+# configs.vit.heads=8 \
+# configs.vit.mlp_dim=512 \
+# configs.vit.dim_head=32 \
+# # configs.debug=True \
+# # trainer.devices=1 \
+# # callbacks.rich_progress_bar.refresh_rate=0 \
 
 
 
@@ -156,5 +244,85 @@ configs.vit.droppath=0.1 \
 
 
 
+
+
+
+
+
+
+# python src/train.py -m \
+# trainer=ddp \
+# task_name=3000_M1 \
+# configs.dataset_loader=fast \
+# configs.train_dataset=\'kinetics_train_7,ava_train_7,avaK_train_7\' \
+# configs.test_dataset=\'ava_val_8\' \
+# configs.ava.predict_valid=True \
+# configs.bottle_neck="conv2k" \
+# configs.ava.gt_type="all" \
+# configs.ava.distil_type="both_bce" \
+# configs.lr=1e-3 \
+# configs.mask_type_test="zero" \
+# configs.frame_length=125 \
+# configs.max_people=5 \
+# configs.extra_feat.enable=\'\' \
+# configs.extra_feat.pose_shape.mid_dim=229 \
+# configs.extra_feat.pose_shape.en_dim=256 \
+# configs.extra_feat.relative_pose.mid_dim=128 \
+# configs.extra_feat.relative_pose.en_dim=256 \
+# configs.use_relative_pose=True \
+# configs.use_optimized_pose=False \
+# configs.in_feat=256 \
+# configs.weight_decay=5e-2 \
+# trainer.gradient_clip_val=2.0 \
+# configs.mask_ratio=0.2 \
+# configs.masked=False \
+# configs.train_batch_size=4 \
+# configs.test_batch_size=4 \
+# configs.train_num_workers=4 \
+# configs.test_num_workers=4 \
+# trainer.accumulate_grad_batches=8 \
+
+
+
+
+# python src/train.py -m \
+# hydra/launcher=submitit_slurm \
+# launcher=slurm \
+# trainer=ddp \
+# task_name=3000_M2 \
+# configs.dataset_loader=fast \
+# configs.train_dataset=\'kinetics_train_7,ava_train_7,avaK_train_7\' \
+# configs.test_dataset=\'ava_val_8\' \
+# configs.ava.predict_valid=True \
+# configs.bottle_neck="conv2k" \
+# configs.ava.gt_type="all" \
+# configs.ava.distil_type="both_bce" \
+# configs.lr=1e-3,2e-3 \
+# configs.mask_type_test="zero" \
+# configs.frame_length=125 \
+# configs.max_people=5 \
+# configs.extra_feat.enable=\'\' \
+# configs.extra_feat.pose_shape.mid_dim=229 \
+# configs.extra_feat.pose_shape.en_dim=256 \
+# configs.extra_feat.relative_pose.mid_dim=128 \
+# configs.extra_feat.relative_pose.en_dim=256 \
+# configs.use_relative_pose=True \
+# configs.use_optimized_pose=False \
+# configs.in_feat=256 \
+# configs.weight_decay=5e-2 \
+# trainer.gradient_clip_val=2.0 \
+# configs.mask_ratio=0.2 \
+# configs.masked=False \
+# configs.train_batch_size=4 \
+# configs.test_batch_size=4 \
+# configs.train_num_workers=4 \
+# configs.test_num_workers=4 \
+# trainer.accumulate_grad_batches=8 \
+# configs.vit.depth=3,9 \
+# configs.vit.heads=8,12 \
+# configs.vit.mlp_dim=128,512 \
+# configs.vit.dim_head=32,128 \
+# configs.vit.dropout=0.1 \
+# configs.vit.droppath=0.1 \
 
 
